e5e872d89bff97b7a3cab7310a6239bb17da3bb9
Maksim Haurylenka <mhaurylenka@gomel.iba.by>
2021-01-14 14:56:19 +0300

    Changed file names to index

--- src/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './Header.css';
-
-const Header = () => <header className="header">Header</header>;
-
-export default Header;
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,3 +1,7 @@
-import Header from './Header'
+import React from 'react';
+
+import './index.css';
+
+const Header = () => <header className="header">Header</header>;
 
 export default Header;
