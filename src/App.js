771818d222d6169f3022fd7a7048e18ea941e1f6
Maksim Haurylenka <mhaurylenka@gomel.iba.by>
2021-01-28 16:18:24 +0300

    Task4: Creating cards list with a read only mode

--- src/App.js
+++ src/App.js
@@ -5,19 +5,53 @@ import Card from './Card';
 
 const App = () => {
 
-  const [cardState, setCardState] = useState({ caption: "Caption 1", text: "Hello World" });
+  const [cardsState, setCardsState] = useState([
+    { id: "id1", caption: "Caption 1", text: "Hello World 1" },
+    { id: "id2", caption: "Caption 2", text: "Hello World 2" },
+    { id: "id3", caption: "Caption 3", text: "Hello World 3" },
+    { id: "id4", caption: "Caption 4", text: "Hello World 4" },
+    { id: "id5", caption: "Caption 5", text: "Hello World 5" },
+    { id: "id6", caption: "Caption 6", text: "Hello World 6" },
+    { id: "id7", caption: "Caption 7", text: "Hello World 7" },
+    { id: "id8", caption: "Caption 8", text: "Hello World 8" },
+    { id: "id9", caption: "Caption 9", text: "Hello World 9" }
+  ]);
+
+  const [readModeState, setReadModeState] = useState({
+    isReadModeActive: false
+  });
 
   const saveChangedState = (tempState) => {
-    setCardState(tempState);
+    const cardId = cardsState.findIndex(card => {
+      return card.id === tempState.id;
+    });
+    let cards = [...cardsState];
+    cards[cardId] = tempState;
+    setCardsState(cards);
   }
+
+  const toggleReadMode = () => {
+    setReadModeState({
+      isReadModeActive: !readModeState.isReadModeActive
+    });
+  };
 
   return (
     <div>
       <Header />
-      <Card
-        cardData = {cardState}
-        onSaveChanges = {saveChangedState}
-      />
+      <div style={{ padding: '10px', margin: '15px' }}>
+        <input type="checkbox" id="only-read" onClick={toggleReadMode} />
+        <label>Только просмотр</label>
+      </div>
+      <div>
+        {cardsState.map((card, index) => {
+          return <Card
+            cardData={card}
+            key={index}
+            onChangeReadMode={readModeState}
+            onSaveChanges={saveChangedState} />
+        })}
+      </div>
     </div>
   );
 };
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiFillSave, AiFillCloseSquare, AiFillEdit } from "react-icons/ai";
 
 import './index.css';
 
 const Card = props => {
 
-    const [checkBoxStyleState, setCheckBoxStyleState] = useState({ isChecked: false })
+    const [checkBoxStyleState, setCheckBoxStyleState] = useState({ isChecked: false });
 
-    const [checkBoxEditModeState, setCheckBoxEditModeState] = useState({ isEditModeActive: false })
+    const [checkBoxEditModeState, setCheckBoxEditModeState] = useState({ isEditModeActive: false });
 
-    const [tempState, setTempState] = useState({...props.cardData});
+    const [tempState, setTempState] = useState({ ...props.cardData });
+
+    useEffect(() => {
+        setTempState({ ...props.cardData });
+        setCheckBoxEditModeState({ isEditModeActive: false });
+    }, [props.onChangeReadMode.isReadModeActive]);
 
     const changeCheckBoxStyle = () => {
         setCheckBoxStyleState({
@@ -25,7 +30,7 @@ const Card = props => {
     }
 
     const changePropertyHandler = (event, property) => {
-        setTempState({...tempState, [property]: event.target.value });
+        setTempState({ ...tempState, [property]: event.target.value });
     }
 
     const saveChangedState = () => {
@@ -34,34 +39,57 @@ const Card = props => {
     }
 
     const cancelChangedState = () => {
-        setTempState({...props.cardData});
+        setTempState({ ...props.cardData });
         toggleEditMode();
+    }
+
+    let buttons = (
+        <div className="buttons">
+            <input className="card-checkbox" type="checkbox" onChange={changeCheckBoxStyle} />
+            <AiFillEdit className="card-square icon-size" onClick={toggleEditMode} />
+        </div>
+    )
+
+    if (checkBoxEditModeState.isEditModeActive) {
+        buttons = (
+            <div className="buttons">
+                <AiFillSave className="icon-size" onClick={saveChangedState} />
+                <AiFillCloseSquare className="icon-size" onClick={cancelChangedState} />
+                <input className="card-checkbox" type="hidden" onChange={changeCheckBoxStyle} />
+                <input className="card-square" type="hidden" onChange={toggleEditMode} />
+            </div>
+        )
+    }
+
+    if (props.onChangeReadMode.isReadModeActive) {
+        buttons = (
+            <div className="buttons">
+                <input className="card-checkbox" type="checkbox" onChange={changeCheckBoxStyle} />
+            </div>
+        )
     }
 
     return (
         <div style={{ display: 'inline-block' }}>
-            {checkBoxEditModeState.isEditModeActive ? (
-                <div className="card-unchecked">
-                    <input className="caption" value={tempState.caption} onChange={(e) => changePropertyHandler(e, "caption")} />
-                    <div className="buttons">
-                        <AiFillSave className="icon-size" onClick={saveChangedState} />
-                        <AiFillCloseSquare className="icon-size" onClick={cancelChangedState} />
-                        <input className="card-checkbox" type="hidden" onChange={changeCheckBoxStyle} />
-                        <input className="card-square" type="hidden" onChange={toggleEditMode} />
-                    </div>
-                    <hr />
-                    <input type="text" value={tempState.text} onChange={(e) => changePropertyHandler(e, "text")} />
-                </div>) : (
-                    <div className={checkBoxStyleState.isChecked ? "card-checked" : "card-unchecked"}>
-                        <h3 className="caption">{tempState.caption}</h3>
-                        <div className="buttons">
-                            <input className="card-checkbox" type="checkbox" onChange={changeCheckBoxStyle} />
-                            <AiFillEdit className="card-square icon-size" onClick={toggleEditMode} />
-                        </div>
-                        <hr />
-                        <p>{tempState.text}</p>
-                    </div>)
-            }
+            <div
+                className={checkBoxEditModeState.isEditModeActive && !props.onChangeReadMode.isReadModeActive ?
+                    "card-unchecked" : (
+                        checkBoxStyleState.isChecked ?
+                            "card-checked" :
+                            "card-unchecked"
+                    )
+                }>
+                {checkBoxEditModeState.isEditModeActive && !props.onChangeReadMode.isReadModeActive ?
+                    <input className="caption" value={tempState.caption} onChange={(e) => changePropertyHandler(e, "caption")} /> :
+                    <h3 className="caption">{tempState.caption}</h3>
+                }
+                {buttons}
+                <hr />
+                {checkBoxEditModeState.isEditModeActive && !props.onChangeReadMode.isReadModeActive ?
+                    <input type="text" value={tempState.text} onChange={(e) => changePropertyHandler(e, "text")} /> :
+                    <p>{tempState.text}</p>
+                }
+            </div>
         </div>
     );
 }
